e6343c1afc2261fee123ade6c5e0155eab2b691c
Malik Ubaydullaev <95644301+Malik-Ubaydullaev@users.noreply.github.com>
2021-12-15 00:28:50 +0500

    Create arithmetic_op04.py
    first commit

---
+++ arithmetic_op04.py
@@ -0,0 +1,13 @@
+number = 184
+x1 = number // 100
+x2 = number // 10 % 10
+x3 = number % 10
+answer = x1 + x2 + x3
+print(answer)
+
+#number = input()
+#x1 = int(number) // 100;
+#x2 = int(number) // 10 % 10
+#x3 = int(number) % 10
+#answer = x1 + x2 + x3
+#print(answer)
