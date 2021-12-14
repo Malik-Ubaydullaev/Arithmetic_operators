fb52f093a1124cb00650801a28a1f78fe8eb2ff9
Malik Ubaydullaev <95644301+Malik-Ubaydullaev@users.noreply.github.com>
2021-12-15 00:20:34 +0500

    Update arithmetic_op03.py
    second commit

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,3 @@
 number = 25
-answer = str(number)
-print(answer[1], answer[0], sep='')
-
-#number = input()
-#answer = number[1]+number[0]
-#print(answer)
+answer = str(number)[1]+str(number)[0]
+print(answer)
