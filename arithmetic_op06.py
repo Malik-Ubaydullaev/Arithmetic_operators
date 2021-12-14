e7938ee845f75e977f3869a0cf36b81eb98c9b2a
Malik Ubaydullaev <95644301+Malik-Ubaydullaev@users.noreply.github.com>
2021-12-15 00:43:06 +0500

    Create arithmetic_op06.py
    first commit

---
+++ arithmetic_op06.py
@@ -0,0 +1,9 @@
+number = 30
+answer = number % 2
+number = number / 2
+print(answer, number, sep=' ')
+
+#number = input()
+#answer = int(number) % 2
+#number = int(number) / 2
+#print(answer, number, sep=' ')
